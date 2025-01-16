9342ff033a29ae9f50538cef67b4e0a046174fd4
Daniel ICE <1119526092@qq.com>
2025-01-16 18:12:15 +0800

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,1 @@
-// export const PYODIDE_SCRIPT = 'https://cdn.jsdelivr.net/npm/pyodide@0.26.4/pyodide.min.js'
-export const PYODIDE_SCRIPT = 'https://storage.360buyimg.com/mf.jd.com/pyodide0.26.4/pyodide.js'
+export const PYODIDE_SCRIPT = 'https://cdn.jsdelivr.net/npm/pyodide@0.26.4/pyodide.min.js'
